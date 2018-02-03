6e7b252a65afbe5faeeb215c1d4f1e00ff8f46f1
sheyla fuentes <sgf763@mail.missouri.edu>
2018-02-03 00:02:21 -0600

    Initial Commit

---
+++ Swift Lengauge Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Lengauge Basics
+//
+//  Created by Sheyla Astete on 2/3/18.
+//  Copyright © 2018 tech innovator. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
